de8eb27e03f21a04fdb2d4daba9adf66aa7759b8
khanhhua165 <billiejoe165@gmail.com>
2021-02-19 16:32:22 +0200

    add sidebar

--- src/components/ClothesItems.tsx
+++ src/components/ClothesItems.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import {
   NavLink,
   Redirect,
@@ -24,6 +24,16 @@ export interface Item {
 }
 
 const ClothesItems = (props: ClothesItemsProps) => {
+  const [sideBarButton, setsideBarButton] = useState("hidden");
+  const handleClick = (e: React.MouseEvent) => {
+    // e.preventDefault();
+    if (sideBarButton === "hidden") {
+      setsideBarButton("");
+    } else {
+      setsideBarButton("hidden");
+      console.log(sideBarButton);
+    }
+  };
   const allItems = useContext(ItemsContext);
   const currentItems = allItems[props.match.params.item];
   if (!currentItems) return null;
@@ -32,13 +42,15 @@ const ClothesItems = (props: ClothesItemsProps) => {
     ...Array(Math.ceil(currentItems.length / itemsPerPage)).keys(),
   ].map((page) => page + 1);
   const pageNavigation = totalPage.map((page) => (
-    <NavLink key={page} to={`${props.match.url}/${page}`}>
-      {page}
+    <NavLink key={page} to={`${props.match.url}/${page}`} onClick={handleClick}>
+      <div className="text-center rounded-lg hover:bg-blue-300 hover:border-red-500">
+        {page}
+      </div>
     </NavLink>
   ));
   return (
     <>
-      <div className="grid grid-cols-1 gap-3 px-2 mt-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+      <div className="grid grid-cols-1 gap-3 px-4 pt-16 mt-4 sm:pr-2 sm:pl-56 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
         <Switch>
           <Route exact path={props.match.url}>
             <Redirect to={`${props.match.url}/1`} />
@@ -51,8 +63,46 @@ const ClothesItems = (props: ClothesItemsProps) => {
           />
         </Switch>
       </div>
-      <div className="flex flex-wrap w-4/5 mx-auto space-x-2">
-        {pageNavigation}
+      <div
+        className={`fixed bottom-0 left-0 z-10 ${sideBarButton} overflow-y-scroll bg-gray-300 sm:block w-52 sidebar-nav`}
+      >
+        <div className="grid w-full grid-cols-4 gap-0.5 px-2 py-2">
+          {pageNavigation}
+        </div>
+      </div>
+      <div
+        onClick={handleClick}
+        className="fixed text-gray-100 bg-blue-400 rounded-full w-14 h-14 sm:hidden bottom-4 right-4"
+      >
+        {sideBarButton === "hidden" ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 8h16M4 16h16"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        )}
       </div>
     </>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,8 +4,8 @@ import { NavLink } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   return (
-    <div className="bg-gray-100">
-      <div className="flex justify-center max-w-6xl px-3 py-2 mx-auto space-x-6 text-gray-700">
+    <div className="fixed top-0 left-0 z-20 w-full h-16 bg-gray-100 shadow-md">
+      <div className="flex items-center justify-center h-full max-w-6xl mx-auto space-x-6 font-semibold text-gray-700">
         <NavLink to="/beanies">
           <NavItem itemName="Beanies" />
         </NavLink>
