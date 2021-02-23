e39446ac6e59ff7738dd2371c88e5f6be85e8bbb
khanhhua165 <billiejoe165@gmail.com>
2021-02-23 09:25:15 +0200

    edit purge option

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./src/**/*.html", "./src/**/*.tsx"],
+  purge: ["./public/**/*.html", "./src/**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
